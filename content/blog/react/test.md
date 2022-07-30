f57e37854324d9564f73e1772b5e9ffaa03a56d4
mechaniccoder <yuseunghwan94@gmail.com>
2022-07-30 19:09:01 +0900

    init project

---
+++ content/blog/react/test.md
@@ -0,0 +1,7 @@
+---
+title: Hello
+date: 2022-07-30 14:07:22
+category: react
+thumbnail: {}
+draft: false
+---
--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+  title: `mechaniccoder.io`,
+  description: `Blog posted about development things `,
+  author: `mechaniccoder`,
+  introduction: `Software engineer를 향해 달려가는 2년차 Frontend developer입니다.`,
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `mechaniccoder`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
-    linkedin: ``, // Your LinkedIn account
+    linkedin: `승환-유-06006b1aa`, // Your LinkedIn account
     instagram: ``, // Your Instagram account
   },
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'mechaniccoder/my-blog', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
@@ -25,9 +25,8 @@ module.exports = {
     buyMeACoffeeId: 'jbee',
   },
   share: {
-    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+    facebookAppId: '1518658065250898', // Add facebookAppId for using facebook share feature v3.2
   },
   ga: '0', // Add your google analytics tranking ID
   ad: '', // Add your google adsense publisherId `ca-pub-xxxxxxxxxx`
-
 }
